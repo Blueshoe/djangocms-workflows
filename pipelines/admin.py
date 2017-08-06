bb4397cf9106291bce0d5c821c5998cd7c5c6bad
veit <veit@blueshoe.de>
2017-08-06 22:55:49 +0200

    Start admins

--- pipelines/admin.py
+++ pipelines/admin.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from adminsortable2.admin import SortableInlineAdminMixin
 from django.contrib import admin
 
+from .models import PipelineStage
+
 # Register your models here.
+
+
+class PipelineStageInline(SortableInlineAdminMixin, admin.TabularInline):
+    # TODO
+    # formset = WorkflowStepInlineFormSet
+    model = PipelineStage
+
+    def get_extra(self, request, obj=None, **kwargs):
+        if obj and obj.pk:
+            return 0
+        return 1
+
+
+class PipelineAdmin(admin.ModelAdmin):
+    # TODO
+    inlines = [PipelineStageInline]
+    list_display = ['name', 'default']
+    fields = ['name', 'is_default']
+
+
+# TODO more required admins: TitleExtensionAdmin, PageAdmin
