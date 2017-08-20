247907f5638c2b6a85774d876b9edda0dcd1f26c
veit <veit@blueshoe.de>
2017-08-20 23:28:25 +0200

    djangocms-workflows-4 Action views - remove print

--- example/configuration/settings/base.py
+++ example/configuration/settings/base.py
@@ -22,7 +22,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
