c088f0d0fb773de31a974a625977d8811ff8eac9
veit <veit@blueshoe.de>
2017-08-21 14:11:02 +0200

    djangocms-workflows-14 Refactoring - handlers: organize imports, refactor, remove unnecessary comments

--- workflows/signals/handlers.py
+++ workflows/signals/handlers.py
@@ -4,79 +4,23 @@ from django.dispatch import receiver
 
 from cms.signals import post_obj_operation
 
-from workflows.models import Action
+from ..models import Action
 
 
-# self._send_post_page_operation(
-#                 request,
-#                 operation=operations.PUBLISH_PAGE_TRANSLATION,
-#                 token=operation_token,
-#                 obj=page,
-#                 translation=page.get_title_obj(language=language),
-#                 successful=all_published,
-#             )
-#
-# post_obj_operation.send(
-#             sender=self.__class__,
-#             operation=operation,
-#             request=request,
-#             token=token,
-#             **kwargs
-#         )
-
-# @receiver(post_publish)  # cannot get user from this one unfortunately
+# @receiver(post_publish)  # cannot easily get user from this signal unfortunately
 @receiver(post_obj_operation)
 def close_moderation_request(sender, request=None, operation=None, translation=None, successful=None, **kwargs):
-    if operation != PUBLISH_PAGE_TRANSLATION:
-        return
-
-    if not successful:
-        return
-
     current_request = Action.get_current_request(translation)
 
-    if current_request is None:
-        return
+    if all((operation == PUBLISH_PAGE_TRANSLATION, successful, current_request)):
+        if not current_request.is_publishable():
+            raise ValueError('Page is not publishable!')
 
-    if not current_request.is_publishable():
-        raise ValueError('Page is not publishable!')
-
-    current_action = current_request.last_action()
-    current_action.add_child(
-        title = translation,
-        workflow=current_action.workflow,
-        # stage=None,
-        # group=None,
-        action_type=Action.PUBLISH,
-        user=request.user,
-        message=''
-    )
-
-
-
-# request = kwargs['request']
-#     operation_type = kwargs['operation']
-#     is_publish = operation_type == PUBLISH_PAGE_TRANSLATION
-#     publish_successful = kwargs.get('successful')
-#
-#     if not is_publish or not publish_successful:
-#         return
-#
-#     page = kwargs['obj']
-#     translation = kwargs['translation']
-#
-#     workflow = get_page_moderation_workflow(page)
-#
-#     if not workflow:
-#         return
-#
-#     active_request = workflow.get_active_request(page, translation.language)
-#
-#     if not active_request:
-#         return
-#
-#     active_request.update_status(
-#         action=ACTION_FINISHED,
-#         by_user=request.user,
-#     )
-
+        current_action = current_request.last_action()
+        current_request.last_action().add_child(
+            title=translation,
+            workflow=current_action.workflow,
+            action_type=Action.PUBLISH,
+            user=request.user,
+            message=''
+        )
